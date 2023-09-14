f6a7412412724428459ec1172c4b91328a627b89
Saba2004 <Sabaev-nik29@yandex.ru>
2023-09-14 11:22:36 +0300

    Hello first programm on c++

---
+++ Sabaev_AC-22-04/Sabaev_AC-22-04/Sabaev_AC-22-04.cpp
@@ -0,0 +1,17 @@
+﻿// Sabaev_AC-22-04.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    string x;
+    cout << "Hello, what is your name?" << endl;
+    cin >> x;
+    cout<<"Hello, " + x<<endl;
+}
+
+
